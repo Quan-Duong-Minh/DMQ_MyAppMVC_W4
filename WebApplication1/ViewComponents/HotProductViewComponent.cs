29425ba822a407e26af509125c093e48873dcac4
DMQ <minhquanduong777@gmail.com>
2026-09-12 22:35:34 +0700

    Add project files.

---
+++ WebApplication1/ViewComponents/HotProductViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourProjectName.ViewComponents
+{
+    public class HotProductViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
